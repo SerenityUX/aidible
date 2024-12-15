dba373c0971bab5acf24ba4ebf81a9f5a70fe6d6
SerenityUX <thomas@serenidad.app>
2024-12-14 20:22:35 -0800

    transpile packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ['@react-pdf/renderer', 'react-pdf']
-  },
+  transpilePackages: [
+    'pdf-parse',
+    '@react-pdf/renderer',
+    'react-pdf'
+  ],
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
